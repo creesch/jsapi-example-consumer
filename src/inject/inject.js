9f86a47c65290e558706e9817c07abb9b47e7ca8
creesch <creesch@users.noreply.github.com>
2018-05-31 20:44:12 +0200

    Adjust example code to be more robust, add commenting.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -9,12 +9,32 @@ const NAME = "test";
 const addElement = (e) => {
   acknowledge(e);
 
+  const target = e.target.querySelector(`[data-name="${NAME}"]`);
+
+  // Events are received that are intended for all consumers. 
+  // This makes sure the extension only handles intended for it.
+  if(!target) {
+    return;
+  }
+
+  // This container has already been handled by this extension before.
+  // To avoid duplicates it will be ignored. 
+  if(target.classList.contains(`${NAME}-handled`)) {
+    return;
+  }
+
+  // Add a class so the extension on refire can tell this target has been handled before.
+  target.classList.add(`${NAME}-handled`);
+
+  // Create element that will be inserted in the container.
   const element = document.createElement('span');
-  element.textContent = '⚠';
-  element.title = e.detail.type;
-  
+  element.textContent = `⚠ ${e.detail.type}`;
+
+  // Pretty printed detail information the jsAPI send for this container.
+  element.title = JSON.stringify(e.detail, null, 4);
+
   requestAnimationFrame(function() {
-    e.target.querySelector(`[data-name="${NAME}"]`).appendChild(element);
+    target.appendChild(element);
   });
 };
 
@@ -27,9 +47,12 @@ const main = () => {
     return;
   }
 
+  // Will listen for dom elements being added.
   document.addEventListener('reddit', addElement, true);
+  // Will listen for page change events.
   document.addEventListener('reddit.urlChanged', acknowledge, true);
 
+  // let the jsAPI know that this extension wants to interact.
   const meta = document.createElement('meta');
   meta.name = 'jsapi.consumer';
   meta.content = NAME;
